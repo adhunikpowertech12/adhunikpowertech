e1b60b68ebfd7750a0dd69a62a3c29458cb76890
adhunikpowertech <marketing@adhunikpowertech.com>
2025-01-15 17:41:43 +0530

    seo 12

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,9 @@ const nextConfig = {
       ignoreDuringBuilds: true, 
     },
  
-   
+   env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  },
 
   };
   
