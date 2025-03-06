b404a79cca1b58045deb4605a421c51dfe3aa48f
decodive <amanalpha2492@gmail.com>
2025-03-06 16:17:22 +0530

    seo 3

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,12 +3,37 @@ import HeroPage from './HeroPage';
 
 
 export const metadata = {
-
   title: "Adhunik Powertech | HVAC & Air Cooling Systems Gurugram",
-  
-  description: "Discover Adhunik Powertech Private Limited, a leader in HVAC solutions and air cooling systems. Based in Gurugram, India, we specialize in innovative, turnkey projects for industrial and commercial cooling needs",
-
+  description: "Discover Adhunik Powertech Private Limited, a leader in HVAC solutions and air cooling systems. Based in Gurugram, India, we specialize in innovative, turnkey projects for industrial and commercial cooling needs.",
+  keywords: [
+    "HVAC solutions",
+    "air cooling systems",
+    "industrial cooling",
+    "commercial cooling solutions",
+    "Adhunik Powertech",
+    "energy-efficient cooling",
+    "Gurugram cooling systems"
+  ],
+  openGraph: {
+    title: "Adhunik Powertech | HVAC & Air Cooling Systems Gurugram",
+    description: "Discover Adhunik Powertech Private Limited, a leader in HVAC solutions and air cooling systems. Based in Gurugram, India, we specialize in innovative, turnkey projects for industrial and commercial cooling needs.",
+    url: "https://www.adhunikpowertech.com/",
+    siteName: "Adhunik Powertech", 
+    locale: "en_IN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Adhunik Powertech | HVAC & Air Cooling Systems Gurugram",
+    description: "Discover Adhunik Powertech Private Limited, a leader in HVAC solutions and air cooling systems. Based in Gurugram, India, we specialize in innovative, turnkey projects for industrial and commercial cooling needs.",
+    creator: "@AdhunikPowertech",
+  },
+  alternates: {
+    canonical: "https://adhunikpowertech.com/",
+  },
 };
+
+
 
 
 
