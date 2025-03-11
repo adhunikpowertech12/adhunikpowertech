1abeb3e6ecf865086d7d317ca57bca59b953069e
decodive <amanalpha2492@gmail.com>
2025-03-11 13:40:11 +0530

    POWERTECH 9

---
+++ src/app/glow-signage-&-emergency-lights/GlowSignEmergencyLight.js
@@ -0,0 +1,576 @@
+"use client";
+
+export default function GlowSignEmergencyLight() {
+  return (
+    <>
+     <div className="row mt-32 font-sans">
+
+
+<div className="row">
+  <h1 className=" w-full text-center font-sans   text-3xl md:text-5xl  text-sky-700 uppercase"> Glow Signage and Emergency Lights </h1>
+
+  <div className=" h-full flex flex-col md:flex-row text-center md:p-5 bg-[#F4F4F4] m-4">
+
+    <div className="md:w-[40%] w-auto">
+      <div className="flex items-center justify-center h-full">
+        <div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/10 rounded-xl">
+            <div className="w-auto">
+              <div className="w-full object-cover transition-transform duration-500 group-hover:scale-125 rounded-xl">
+                <img
+                  className="  md:h-[28rem]  object-center object-cover  "
+                  fetchPriority="high"
+                  decoding="async"
+                  alt="Glow Signage and Emergency Lights"
+                  src=" https://www.kohler-ups.co.uk/wp-content/uploads/2019/07/Lighting-p36-37.jpg"
+                />
+              </div>
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h2 className="font-dmserif text-2xl font-bold text-white">  Glow Signage and Emergency Lights  </h2>
+              <p className="mb-3 pt-2 text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Stay cool and comfortable all summer long with our efficient air cooler, designed for optimal airflow and energy savings.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="md:w-[60%] w-auto h-full p-2 m-2 md:p-5 md:m-5  md:text-justify">
+      <h2 className="text-[6vw] md:text-4xl mb-3 hidden md:block"> Glow Signage and Emergency Lights</h2>
+      <p className="md:text-justify  py-2">
+      Glow signage and emergency lights are critical components of safety systems in
+various environments, including commercial buildings, public spaces, and
+residential areas. They provide crucial information and illumination during
+emergencies, ensuring safe evacuation and guiding occupants to safety.
+      </p>
+
+
+      <div className="card text-left">
+      <p className=" text-4xl pt-2  text-left "> Glow Signage </p>
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+
+
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         Glow signage:
+          
+           </span>  
+         
+           refers to illuminated signs that use photoluminescent materials or
+integrated lighting to enhance visibility, especially in low-light or emergency
+situations.
+
+
+          </li>
+
+       
+          
+        
+
+
+        </ul>
+      </div>
+
+      <div className="card text-left">
+      <p className=" text-4xl pt-2  text-left "> Key Features</p>
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+
+
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         
+         Visibility:
+          
+           </span>  
+         Designed to be highly visible in darkness, ensuring occupants can
+           quickly identify exits and safety routes.
+
+
+          </li>
+
+
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         
+         Durability:
+          
+           </span>  
+            Often made from robust materials that withstand wear and
+           environmental factors, maintaining visibility over time.
+
+          </li>
+
+       
+          
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         
+         Non-Electric Options:
+           </span>  
+          Photoluminescent signs absorb light and glow in the dark,
+           requiring no power source and functioning effectively during power outages.
+
+          </li>
+
+       
+          
+        
+
+
+        </ul>
+      </div>
+
+      <div className="card text-left">
+      <p className=" text-4xl pt-2  text-left "> Common Applications</p>
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+
+
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         Exit signs
+          
+           </span>  
+         
+        
+
+
+          </li>
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         Directional signs
+
+          
+           </span>  
+         
+        
+
+
+          </li>
+          <li>
+         <span className=" font-sans font-bold"> 
+         
+         Safety information signs (e.g., fire exits, assembly points)
+          
+           </span>  
+         
+        
+
+
+          </li>
+
+       
+          
+        
+
+
+        </ul>
+      </div>
+
+
+      <div className="show_m mt-5">
+        <button
+          className="rounded-md bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 mt-5"
+          onClick={() => {
+            window.location.href = 'mailto:sales@adhunikpowertech.com?subject=Enquiry%20Now&body=Hello, I would like to enquire about your services.';
+          }}
+        >
+          Enquiry Now
+        </button>
+
+
+
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+<div className="row">
+
+
+  <div className=" h-full flex flex-col-reverse md:flex-row text-center md:p-5 bg-[#F4F4F4] m-4 items-center">
+
+
+
+    <div className="md:w-[60%] w-auto h-full p-2 m-2 md:p-5 md:m-5  md:text-justify">
+      <h2 className="text-[6vw] md:text-4xl mb-3    md:text-left"> Emergency Lights</h2>
+
+     
+      
+<p>Emergency lights are battery-powered lighting fixtures
+designed to illuminate areas during power failures or
+emergencies. They provide essential illumination to guide
+people safely out of a building.</p>
+
+
+
+
+
+      <div className="card text-left">
+
+      <h2 className="text-[6vw] md:text-4xl mb-3    text-left"> Key Features:</h2>
+ 
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+          <li>
+
+         <span className=" font-sans font-bold">
+            
+         Automatic Activation: 
+           </span> 
+           Emergency lights typically
+activate automatically when the main power supply fails,
+ensuring immediate illumination.
+
+
+          </li>
+          <li>
+
+         <span className=" font-sans font-bold">
+            
+         Battery Backup:
+           </span> 
+          Equipped with rechargeable batteries
+           that ensure functionality during power outages.
+
+          </li>
+
+          <li>
+
+         <span className=" font-sans font-bold">
+            
+         Variety of Designs:
+           </span> 
+         Available in different configurations,
+including wall-mounted, ceiling-mounted, and portable
+options.
+
+          </li>
+
+      
+        
+    
+        
+        </ul>
+      </div>
+
+
+   
+      <div className="card text-left">
+
+      <h2 className="text-[6vw] md:text-4xl mb-3    text-left"> Common Applications </h2>
+ 
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+      
+      
+          <li>
+
+         <span className=" font-sans font-bold">
+         Hallways and stairwells
+
+
+           </span> 
+        
+
+
+          </li>
+          <li>
+
+         <span className=" font-sans font-bold">
+            
+         Exit routes
+           </span> 
+        
+
+
+          </li>
+          <li>
+
+         <span className=" font-sans font-bold">
+            
+         Assembly areas and gathering points
+           </span> 
+        
+
+
+          </li>
+   
+
+      
+        
+    
+        
+        </ul>
+      </div>
+
+
+   
+
+
+
+
+
+
+
+   
+    </div>
+
+
+    <div className="md:w-[40%] w-auto">
+      <div className="flex items-center justify-center h-full">
+        <div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/10 rounded-xl">
+            <div className="w-auto">
+              <div className="w-full object-cover transition-transform duration-500 group-hover:scale-125 rounded-xl">
+                <img
+                  className="  h-[20rem] md:h-[28rem]  object-center object-cover  "
+                  fetchPriority="high"
+                  decoding="async"
+                  alt="Glow Signage and Emergency Lights"
+                  src=" https://www.intersafe.co.uk/wp-content/uploads/2021/12/workplace-emergency-lighting-testing-1024x427.jpg "
+                />
+              </div>
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h2 className="font-dmserif text-2xl font-bold text-white">  Glow Signage and Emergency Lights  </h2>
+              <p className="mb-3 pt-2 text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Stay cool and comfortable all summer long with our efficient air cooler, designed for optimal airflow and energy savings.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+<div className="row">
+
+
+  <div className=" h-full flex flex-col md:flex-row text-center md:p-5 bg-[#F4F4F4] m-4">
+
+
+    <div className="md:w-[40%] w-auto">
+      <div className="flex items-center justify-center h-full">
+        <div>
+          <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/10 rounded-xl">
+            <div className="w-auto">
+              <div className="w-full object-cover transition-transform duration-500 group-hover:scale-125 rounded-xl">
+                <img
+                  className=" h-[20rem] md:h-[28rem]  object-center object-cover  "
+                  fetchPriority="high"
+                  decoding="async"
+                  alt="Glow Signage and Emergency Lights"
+                  src=" https://5.imimg.com/data5/SELLER/Default/2023/5/310175698/UO/MQ/AF/12739366/industrial-emergency-exit-light-500x500.jpg "
+                />
+              </div>
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+            <div className="absolute inset-0 flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+              <h2 className="font-dmserif text-2xl font-bold text-white">  Glow Signage and Emergency Lights  </h2>
+              <p className="mb-3 pt-2 text-sm italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                Stay cool and comfortable all summer long with our efficient air cooler, designed for optimal airflow and energy savings.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div className="md:w-[60%] w-auto h-full p-2 m-2 md:p-5 md:m-5  md:text-justify">
+      <h2 className="text-[6vw] md:text-4xl mb-3 hidden md:block"> Benefits of Glow Signage and
+      Emergency Lights </h2>
+    
+    
+
+
+
+
+
+
+      <div className="card text-left">
+   
+
+        <ul className=" list-disc py-3 space-y-1  ps-4">
+
+          <li>
+         <span className=" font-sans font-bold">
+         Enhanced Safety:
+          </span>
+      By providing clear guidance and illumination, these
+features significantly improve safety during emergencies, helping
+occupants navigate safely.
+
+
+          </li>
+
+
+          <li>
+         <span className=" font-sans font-bold">
+      
+         Regulatory Compliance:
+          </span>
+          Many jurisdictions mandate the installation
+of glow signage and emergency lighting in commercial buildings,
+ensuring adherence to safety codes.
+
+          </li>
+          <li>
+         <span className=" font-sans font-bold">
+         Increased Visibility:
+          </span>
+
+    Glow signage is especially valuable in smoke-filled
+          or dark environments, where traditional lighting may fail.
+
+
+          </li>
+          <li>
+         <span className=" font-sans font-bold">
+              Cost-Effective:
+          </span>
+
+   
+  Photoluminescent signs require no electricity,
+reducing energy costs and maintenance associated with traditional
+illuminated signs.
+
+
+          </li>
+          <li>
+         <span className=" font-sans font-bold">
+         Peace of Mind:
+          </span>
+
+   
+           The presence of reliable signage and lighting helps
+          occupants feel safer and more prepared for emergencies.
+
+
+          </li>
+
+ 
+    
+
+        
+        </ul>
+      </div>
+
+
+     
+     
+<p className="space-y-1  ps-4"> Glow signage and emergency lights are vital components of any
+effective safety strategy. They enhance visibility, facilitate safe
+evacuation, and ensure compliance with safety regulations. By investing
+in high-quality signage and emergency lighting, property owners can
+create a safer environment for occupants, providing peace of mind and
+improving overall safety during emergencies. </p>
+
+   
+
+  
+
+
+
+
+
+
+
+   
+    </div>
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<div className="row flex-col flex justify-center items-center py-10">
+
+
+<div className="flex items-center justify-center  md:py-10  ">
+
+<p className="mb-4 text-3xl font-extrabold text-center md:text-5xl lg:text-6xl">
+<span className="text-transparent  bg-clip-text bg-gradient-to-r to-[#f7b733] from-[#fc4a1a]">    Choosing the Right Glow Signage and Emergency Lights</span> 
+</p>
+</div>
+
+
+<div className="container  text-center py-5 md:px-10 px-3">
+<p> The appropriate type of Glow Signage and Emergency Lights depends on the specific hazards present in your home, office, or workplace. For example, if you frequently cook with oils, a Class K extinguisher is essential. Consult with a fire safety expert to determine the best options for your needs.
+</p>
+
+<div className="show_m mt-5">
+<button
+className="rounded-md bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60 mt-5"
+onClick={() => {
+window.location.href = 'tel:+919870284753'; // Replace with the actual phone number
+}}
+>
+
+Call Us Now
+
+</button>
+
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+    </>
+  )
+}
