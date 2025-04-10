b220a97bdd0d4d8ca3734a1ac7dc90225f103600
decodive <amanalpha2492@gmail.com>
2025-04-10 16:52:54 +0530

    adhunik powertech 70

--- src/app/panel-air-conditioners/page.js
+++ src/app/panel-air-conditioners/page.js
@@ -3,7 +3,7 @@ import PanelAirConditioners from "./PanelAirConditioners"
 
 export const metadata = {
 
-  title: "Panel Air Conditioner Manufacturer in Delhi - Best Industrial Panel Air Conditioner",
+  title: "Best Panel Air Conditioner in India | Adhunik Powertech",
 
   description: "Adhunik Powertech, a leading manufacturer of industrial panel air conditioners in Gurugram, India, offers advanced cooling solutions for PLCs, Electric panels, CNC machines, etc. Contact us for a free quote!",
   
