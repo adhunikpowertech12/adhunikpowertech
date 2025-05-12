ab602e9d6bc058443d8b441073033a2bf89cf84e
decodive <amanalpha2492@gmail.com>
2025-05-12 17:37:53 +0530

    adhunik p

--- src/app/ducted-air-cooler/page.js
+++ src/app/ducted-air-cooler/page.js
@@ -3,9 +3,9 @@ import DuctedAirCoolerPage from "./DuctedAirCooler"
 
 export const metadata = {
 
-  title: "Best Ducted Air Coolers for Warehouses | Adhunik Powertech",
+  title: "Duct Cooler Manufacturer Delhi NCR India | Adhunik Powertech",
 
-  description: "We manufacture high-performance industrial ducting air coolers in Delhi NCR for factories, warehouses, and more. Offering durable and energy-efficient solutions, including stainless steel and plastic models. Get a free quote today!",
+  description: "Adhunik Powertech: Get durable, energy-saving duct air coolers in Delhi NCR & India, including Stainless Steel 304. Serving Gurugram, Noida, Faridabad & more. Quote now!",
   
 }
 
