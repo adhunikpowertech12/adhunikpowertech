aa26f192e7ad2b81d8e3ac57cfcab90189321c4f
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-05 15:25:23 +0530

    hvac added 4

--- src/app/industrial-cooler/page.js
+++ src/app/industrial-cooler/page.js
@@ -4,8 +4,8 @@ import IndustrialCooler from "./IndustrialCooler";
 export const metadata = {
    title: "Industrial Cooler Manufacturer Delhi NCR | Tent & Heavy Duty",
   description: "Top industrial coolers in Gurugram & Delhi NCR by Adhunik Powertech. Heavy-duty & tent coolers for factories & commercial spaces. Get a free quote!",
+  
   keywords: [
-    
     "Industrial Cooler",
     "Industrial Cooler Manufacturer",
     "Heavy Duty Cooler",
