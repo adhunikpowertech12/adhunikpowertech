0b001c3cb8c6699957759b69a2ca8b1499ff282a
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-10 11:59:51 +0530

    url cona 2

--- src/app/what-is-hvac/page.js
+++ src/app/what-is-hvac/page.js
@@ -4,7 +4,7 @@ export const metadata = {
   title: "What is HVAC? A Comprehensive Guide for Beginners",
   description: " Learn about HVAC systems: their full form, components (heating, ventilation, air conditioning), how they work, and different types. This beginner-friendly guide explains HVAC in simple terms.",
   alternates: {
-    canonical: "https://www.adhunikpowertech.com/what-is-hvac?",  
+    canonical: "https://www.adhunikpowertech.com/what-is-hvac",  
   },
 }
 
