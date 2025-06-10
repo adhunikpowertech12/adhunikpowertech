3c546e03bf35e0c5dfba785cc749b48b0ad085c1
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-10 12:46:32 +0530

    url cona 10

--- src/app/hvac/HvacProduct.js
+++ src/app/hvac/HvacProduct.js
@@ -779,6 +779,7 @@ export default function HvacProduct() {
                 <img
                   className="object-cover md:w-[50%] p-8 md:h-[50%]"
                   src={card.imgSrc}
+                  loading="lazy"
                   alt={`${card.clientName} project`}
                 />
 
