9d5d39753ac6e563357217710d5b445d597cda56
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-06-26 12:02:00 +0530

    phone view testing 2

--- src/app/bldc-fans/BldcFans.js
+++ src/app/bldc-fans/BldcFans.js
@@ -508,8 +508,8 @@ export const BldcFans = () => {
 
 
   return (
-    <div className="min-h-screen  w-full bg-gray-50">
-      <main className="container-fluid mx-auto px-4 md:px-14 py-12 mt-32">
+    <div className="min-h-screen   bg-gray-50">
+      <main className="container mx-auto px-4 md:px-14 py-12 mt-32">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mb-20">
           {/* Gallery Section */}
           <div className="space-y-6">
