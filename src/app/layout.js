021adc0dc76e12f9a22d06df28019acb1520ba0d
Aman Sharma <60314850+amansharma32@users.noreply.github.com>
2025-09-01 17:09:43 +0530

    landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-"use client"
+ 
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -12,6 +12,82 @@ import Security from "@/components/Security";
 
 
 
+export const metadata = {
+  title: {
+
+    default: " Top & Best HVAC Company In India - Adhunik Powertech",
+    template: "%s"
+
+  },
+  description: "Adhunik Powertech: Your trusted HVAC contractor in Delhi. Expert installations, repairs, and maintenance for a comfortable indoor environment. Contact us now!",
+
+  
+  keywords: [
+    "HVAC Solutions",
+    "Industrial Air Cooling",
+    "Commercial Air Cooling Systems",
+    "Air Washer Systems",
+    "Ducted Air Coolers",
+    "Ventilation Exhaust Fans",
+    "Panel Air Conditioners",
+    "Oil and Coolant Chillers",
+    "Air Handling Units",
+    "Air Shower Systems",
+    "Energy-Efficient Cooling Solutions",
+    "Industrial Ventilation Systems",
+    "Commercial HVAC Systems",
+    "HVAC contractors",
+    "hvac company in delhi NCR",
+    "hvac company India"
+  ],
+
+  robots: "index,follow",
+  appleMobileWebAppCapable: "yes",
+  appleMobileWebAppTitle: "Adhunik Powertech",
+  appleMobileWebAppStatusBarStyle: "default",
+  robots: "index, follow",
+
+  alternates: {
+    canonical: "https://www.adhunikpowertech.com/",
+  },
+
+  twitter: {
+    site: "@adhunikpowertech",
+    title: "Adhunik Powertech | Top HVAC & Air Cooling Systems Gurugram",
+    description: "Discover Adhunik Powertech Private Limited, a leader in HVAC solutions and air cooling systems. Based in Gurugram, India, we specialize in innovative, turnkey projects for industrial and commercial cooling needs.",
+    card: "summary",
+    image: "https://adhunikpowertech.com/apple-touch-icon.png",
+    imageAlt: "Adhunik Powertech - HVAC & Air Cooling Solutions"
+  },
+  openGraph: {
+    title: "Adhunik Powertech",
+    type: "website",
+    url: "https://www.adhunikpowertech.com/",
+    siteName: "Adhunik Powertech - HVAC Experts",
+    image: "https://adhunikpowertech.com/apple-touch-icon.png",
+    locale: "en_IN",
+    creator: "Adhunik Powertech",
+
+
+    images: [
+      {
+        url: "https://adhunikpowertech.com/apple-touch-icon.png",
+        width: 1200,
+        height: 630,
+        alt: "Adhunik Powertech HVAC Solutions"
+      }
+    ]
+  },
+
+  additionalMeta: [
+    { httpEquiv: "X-UA-Compatible", content: "IE=EmulateIE7" },
+    { httpEquiv: "Accept-CH", content: "DPR, Viewport-Width, Width, Save-Data" }
+  ],
+
+  link: [
+    { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" },
+  ]
+};
  
 
 
@@ -55,7 +131,6 @@ export default function RootLayout({ children }) {
           gtag('config', 'AW-16494041391');
         `}
       </Script>
-
 
         <GoogleTagManager gtmId="GTM-W994SKBG" />
         
@@ -105,6 +180,7 @@ export default function RootLayout({ children }) {
         <Analytics />
         <SpeedInsights />
         <Header />
+        
         <WatsapButton phoneNumber="+919599050534" />
 
         <noscript>
--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -1,6 +1,7 @@
+"use client"
 import React, { useEffect, useState, useCallback, useRef } from 'react';
 
-// A simple and elegant SVG icon for the lock
+
 const LockIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5 mr-2">
     <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
