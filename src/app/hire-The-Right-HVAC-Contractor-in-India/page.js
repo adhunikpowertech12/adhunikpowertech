9a4b74890fb9353a243a66e3cdb3d7e27281aafc
Adhunik Powertech <marketing@adhunikpowertech.com>
2025-10-06 13:10:08 +0530

    Update page.js
    ALL DONE

--- src/app/hire-The-Right-HVAC-Contractor-in-India/page.js
+++ src/app/hire-The-Right-HVAC-Contractor-in-India/page.js
@@ -3,11 +3,11 @@ import HvacRightContractor from "./HvacRightContractor";
 
 export const metadata =
 {
-  title:'Hire the Right HAVC Contractor in India: Tips & Guide | Adhunik Powertech',
-  description:"Looking to hire an HAVC contractor in India? Adhunik Powertech's guide provides essential tips for finding the best HAVC contractor for your installation, repair, or maintenance needs."
+  title:'Hire the Right HVAC Contractor in India: Tips & Guide | Adhunik Powertech',
+  description:"Looking to hire an HVAC contractor in India? Adhunik Powertech's guide provides essential tips for finding the best HVAC contractor for your installation, repair, or maintenance needs."
   ,
     alternates: {
-    canonical:"https://www.adhunikpowertech.com/hire-The-Right-HAVC-Contractor-in-India",  
+    canonical:"https://www.adhunikpowertech.com/hire-The-Right-HVAC-Contractor-in-India",  
   },
 }
 
