10b7353e5008012517ce025e3a4615db7811ab82
Adhunik Powertech <marketing@adhunikpowertech.com>
2025-10-06 12:59:10 +0530

    Update page.js
    ALL DONE

--- src/app/fire-alarm-system/page.js
+++ src/app/fire-alarm-system/page.js
@@ -2,7 +2,7 @@ import FireAlarmSystem from "./FireAlarmSystem";
  
 
 export const metadata = {
-  title: "Fire Alarm System Contrator, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
+  title: "Fire Alarm System Contractor, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
   description:
     "Adhunik FireXpert: Fire Alarm System contractor, supplier & dealer in Delhi NCR, Gurugram, Noida & Jaipur. Get ISI/NFPA-certified design, installation, maintenance & AMC for early fire detection. Contact us!",
   keywords: [
@@ -69,7 +69,7 @@ export const metadata = {
   ],
   
   openGraph: {
-    title: "Fire Alarm System Contrator, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
+    title: "Fire Alarm System Contractor, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
     description:
       "Adhunik FireXpert: Top Fire Alarm System contractor, supplier & dealer in Delhi NCR, Gurugram, Noida & Jaipur. Get ISI/NFPA-certified design, installation, maintenance & AMC for early fire detection. Contact us!",
     url: "https://www.adhunikpowertech.com/fire-alarm-system",  
@@ -91,7 +91,7 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Fire Alarm System Contrator, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
+    title: "Fire Alarm System Contractor, Supplier & Dealer in Delhi NCR, Gurugram | Adhunik FireXpert",
     description:
       "Adhunik FireXpert: Top Fire Alarm System contractor, supplier & dealer in Delhi NCR, Gurugram, Noida & Jaipur. Get ISI/NFPA-certified design, installation, maintenance & AMC for early fire detection. Contact us!",
     image: "https://res.cloudinary.com/dgx5cntyb/image/upload/v1749038480/fire_alarm_scmnlu.webp", 
