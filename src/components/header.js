894b1cc8789e5de8cee9fd4234140d47e9ccbbfa
Captain Ritzz <captainritzz@gmail.com>
2025-12-03 15:19:51 +0530

    Headphone icon size issue resolved

--- src/components/header.js
+++ src/components/header.js
@@ -526,7 +526,7 @@ function Navbar() {
 
                 <div className="row flex flex-col  md:flex-row">
                   <div className="col flex justify-center items-center">   
-                    <svg xmlns="http://www.w3.org/2000/svg" className="px-2 lucide lucide-search-icon lucide-search" onClick={() => setShowSearch(!showSearch)} width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="px-2 lucide lucide-search-icon lucide-search" onClick={() => setShowSearch(!showSearch)} width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
                    
                   </div>
 
@@ -542,7 +542,7 @@ function Navbar() {
                         </svg>
                       ) : (
 
-                        <svg className="md:px-2 lucide lucide-headphones-icon lucide-headphones" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 14h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a9 9 0 0 1 18 0v7a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-headphones-icon lucide-headphones"><path d="M3 14h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a9 9 0 0 1 18 0v7a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3"/></svg>
                        
                       )}
                     </div>
