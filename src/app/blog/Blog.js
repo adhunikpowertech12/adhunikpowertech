bddef371fce4272ca4b102ece7256be878f74d62
shivali <bishnoishivali07@gmail.com>
2026-03-23 17:51:27 +0530

    fix the error

--- src/app/blog/Blog.js
+++ src/app/blog/Blog.js
@@ -350,14 +350,14 @@ export default function Blog() {
     },
     {
       id: 34,
-      title: "AHU vs FFU in Pharma Clean Rooms:Which is Better?",
+      title: "AHU vs FFU in Pharma Clean Rooms: Which is Better?",
       date: "March 2026",
       image:
         "",
       link: "/ahu-vs-ffu-in-pharma-clean-rooms",
       img: "https://res.cloudinary.com/ddkyx2jhh/image/upload/v1774258077/ahuvsffu_pcs3xq.webp",
     },
-    /*{
+    {
       id: 35,
       title: "Top HVAC Contractor in Delhi NCR | Industrial & Commercial HVAC Solutions",
       date: "March 2026",
@@ -365,7 +365,7 @@ export default function Blog() {
         "",
       link: "/top-hvac-contractor-in-delhi-ncr",
       img: "https://res.cloudinary.com/ddkyx2jhh/image/upload/v1774247285/HVACinDelhincr_nioekh.webp",
-    },*/
+    }
   ];
 
   return (
