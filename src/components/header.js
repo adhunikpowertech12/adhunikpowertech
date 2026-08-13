8b62189d50d47072ff00e46901b70003fe85bd07
shivali <bishnoishivali07@gmail.com>
2026-08-13 12:55:35 +0530

    Updated HVAC profile

--- src/components/header.js
+++ src/components/header.js
@@ -471,8 +471,8 @@ useEffect(() => {
                   >
 
                     <a
-                      href="/Adhunik Powertech Project Catalogue.pdf" // The file path in the public folder
-                      download="Adhunik-Powertech-Project-Catalogue"
+                      href="/Adhunik-Powertech-HVAC-Profile.pdf" // The file path in the public folder
+                      download="Adhunik-Powertech-HVAC-Profile"
                       className={`relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold rounded-full group ${isScrolled
                         ? " text-black hover:text-black border-black "
                         : " text-white hover:text-black "
@@ -481,7 +481,7 @@ useEffect(() => {
                       <span className=" w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-gray-200 opacity-[3%]"></span>
                       <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
                       <span className="relative w-full text-left">
-                        Project Catalogue
+                        HVAC Profile
                       </span>
                       <span className="absolute inset-0 rounded-full"></span>
                     </a>
